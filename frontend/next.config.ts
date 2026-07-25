ff2f1880bd8d68c0891bf4260d13106474bb6380
GregorioFerreira-hub <gregorioagostinho@gmail.com>
2026-07-25 01:11:24 +0100

    fix: atualiza importacao do pacote PWA para @ducanh2912/next-pwa

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-import withPWAInit from 'next-pwa';
-
+//import withPWAInit from 'next-pwa';
+import withPWAInit from '@ducanh2912/next-pwa';
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
